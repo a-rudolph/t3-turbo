7876ee524638184dab2cc40179e7bdf5902b721a
a-rudolph <adam.rudolph@live.ca>
2023-06-01 21:31:49 -0400

    move pet page to petId

--- apps/nextjs/src/pages/pets/[petId].tsx
+++ apps/nextjs/src/pages/pets/[petId].tsx
@@ -1,10 +1,22 @@
-import type { InferGetServerSidePropsType, NextPage } from "next/types";
+import type {
+  GetServerSidePropsContext,
+  InferGetServerSidePropsType,
+  NextPage,
+} from "next/types";
 
 import { getPetById } from "@acme/gen-swag";
 
-export const getServerSideProps = async () => {
+export const getServerSideProps = async (
+  ctx: GetServerSidePropsContext<{ petId: string }>,
+) => {
   try {
-    const pets = await getPetById(4);
+    const petIdString = ctx.params?.petId || "";
+
+    const [_petId] = petIdString.split("-");
+
+    const petId = Number(_petId);
+
+    const pets = await getPetById(petId);
     //      ^?
 
     return {
@@ -15,7 +27,7 @@ export const getServerSideProps = async () => {
   } catch (error) {
     return {
       props: {
-        error: JSON.stringify(error),
+        error,
       },
     };
   }
@@ -23,12 +35,12 @@ export const getServerSideProps = async () => {
 
 const PetsPage: NextPage<
   InferGetServerSidePropsType<typeof getServerSideProps>
-> = (props) => {
-  if (!props.pets || props.error) {
+> = ({ pets, error }) => {
+  if (!pets && typeof error === "object" && error !== null) {
     return (
       <div className="container mx-auto px-4 py-8">
         <h1 className="text-lg">Error</h1>
-        {Object.entries(JSON.parse(props.error)).map(([key, value]) => (
+        {Object.entries(error).map(([key, value]) => (
           <p key={key}>
             {key}: {String(value)}
           </p>
@@ -40,7 +52,7 @@ const PetsPage: NextPage<
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-lg">Pets</h1>
-      <p>{props.pets.data?.name}</p>
+      <p>{pets?.data?.name}</p>
     </div>
   );
 };
