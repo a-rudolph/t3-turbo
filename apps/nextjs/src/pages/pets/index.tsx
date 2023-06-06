d8c8de8506329594bc91187639188d9f97e786d0
a-rudolph <adam.rudolph@live.ca>
2023-06-05 23:33:05 -0400

    add pets list page

---
+++ apps/nextjs/src/pages/pets/index.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+
+interface Props {
+  numPets: number;
+}
+
+function PetsPage({ numPets = 10 }: Props) {
+  const petIds = new Array<number>(numPets).fill(null);
+
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="mb-4 text-2xl font-bold">Pets</h1>
+      <ul className="list-disc pl-4">
+        {petIds.map((_, id) => {
+          return (
+            <li key={id} className="mb-2">
+              <Link
+                className="text-blue-500 hover:underline"
+                href={`/pets/${id}`}
+              >
+                Pet {id}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default PetsPage;
