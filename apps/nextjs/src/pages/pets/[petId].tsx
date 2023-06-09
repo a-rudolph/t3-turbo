694b371a389a6f158ae58bd042057cbc75482ecd
a-rudolph <adam.rudolph@live.ca>
2023-06-08 22:43:45 -0400

    replace url with that with name

--- apps/nextjs/src/pages/pets/[petId].tsx
+++ apps/nextjs/src/pages/pets/[petId].tsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 import type {
   GetServerSidePropsContext,
   InferGetServerSidePropsType,
@@ -10,6 +12,8 @@ export const getServerSideProps = async (
   ctx: GetServerSidePropsContext<{ petId: string }>,
 ) => {
   try {
+    console.log("fetching");
+
     const petIdString = ctx.params?.petId || "";
 
     const [_petId] = petIdString.split("-");
@@ -18,6 +22,8 @@ export const getServerSideProps = async (
 
     const pets = await getPetById(petId);
     //      ^?
+
+    // ctx.res.setHeader("Cache-Control", "s-maxage=5, stale-while-revalidate");
 
     return {
       props: {
@@ -36,9 +42,27 @@ export const getServerSideProps = async (
 const PetsPage: NextPage<
   InferGetServerSidePropsType<typeof getServerSideProps>
 > = ({ pets, error }) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (pets?.data.name) {
+      router
+        .replace(`/pets/${pets.data.id}-${pets.data.name.replace(/\s/g, "-")}`)
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+  }, [pets?.data.name]);
+
   const test = async () => {
     const response = await findPetsByStatus(["pending", "available"]);
     console.log(response);
+  };
+
+  const onClick = () => {
+    test().catch((error) => {
+      console.error(error);
+    });
   };
 
   if (!pets && typeof error === "object" && error !== null) {
@@ -53,7 +77,7 @@ const PetsPage: NextPage<
 
         <button
           className="mt-4 rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
-          onClick={test}
+          onClick={onClick}
         >
           test
         </button>
@@ -67,7 +91,7 @@ const PetsPage: NextPage<
       <p>{pets?.data?.name}</p>
       <button
         className="mt-4 rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
-        onClick={test}
+        onClick={onClick}
       >
         test
       </button>
--- apps/nextjs/src/pages/pets/index.tsx
+++ apps/nextjs/src/pages/pets/index.tsx
@@ -1,24 +1,33 @@
+import { type InferGetServerSidePropsType } from "next";
 import Link from "next/link";
 
-interface Props {
-  numPets: number;
-}
+import { findPetsByStatus } from "@acme/gen-swag";
 
-function PetsPage({ numPets = 10 }: Props) {
-  const petIds = new Array<number>(numPets).fill(1);
+export const getServerSideProps = async () => {
+  const response = await findPetsByStatus(["available"]);
 
+  return {
+    props: {
+      pets: response.data,
+    },
+  };
+};
+
+function PetsPage({
+  pets,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="mb-4 text-2xl font-bold">Pets</h1>
       <ul className="list-disc pl-4">
-        {petIds.map((_, id) => {
+        {pets?.map((pet) => {
           return (
-            <li key={id} className="mb-2">
+            <li key={pet.id} className="mb-2">
               <Link
                 className="text-blue-500 hover:underline"
-                href={`/pets/${id}`}
+                href={`/pets/${pet.id}`}
               >
-                Pet {id}
+                {pet.name || pet.id}
               </Link>
             </li>
           );
