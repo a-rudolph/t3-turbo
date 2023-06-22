5e8c8405596232a6fbc576016031ec5208787743
a-rudolph <adam.rudolph@live.ca>
2023-06-21 22:25:43 -0400

    only load unique pets

--- apps/vitejs/src/routes/root.tsx
+++ apps/vitejs/src/routes/root.tsx
@@ -10,7 +10,23 @@ export async function loader() {
   if (!response.data) {
     throw new Error("Failed to load contacts");
   }
-  return { pets: response.data };
+
+  const pets = response.data;
+
+  const uniquePetIds = new Set<number>();
+
+  const uniquePets = pets.filter((pet) => {
+    if (!pet.id) return false;
+
+    if (uniquePetIds.has(pet.id)) {
+      return false;
+    } else {
+      uniquePetIds.add(pet.id);
+      return true;
+    }
+  });
+
+  return { pets: uniquePets };
 }
 
 export default function Root() {
