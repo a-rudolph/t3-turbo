e7454c66d7698e85bd961050e607657c8347369b
a-rudolph <adam.rudolph@live.ca>
2023-06-24 20:13:51 -0400

    add pagination

--- apps/vitejs/src/routes/table.tsx
+++ apps/vitejs/src/routes/table.tsx
@@ -4,6 +4,7 @@ import {
   getCoreRowModel,
   getPaginationRowModel,
   useReactTable,
+  type Table,
 } from "@tanstack/react-table";
 import { Link, useLoaderData, useNavigate } from "react-router-dom";
 
@@ -77,34 +78,69 @@ const PetTable: React.FC = () => {
           ))}
         </tbody>
       </table>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          gap: "1rem",
-          marginTop: "1rem",
-        }}
-      >
-        <button
-          disabled={!table.getCanPreviousPage()}
-          onClick={() => table.previousPage()}
-        >
-          prev
-        </button>
-        <p>
-          Page {table.getState().pagination.pageIndex + 1} of{" "}
-          {table.getPageCount()}
-        </p>
-        <button
-          disabled={!table.getCanNextPage()}
-          onClick={() => table.nextPage()}
-        >
-          next
-        </button>
-      </div>
+      <Pagination table={table} />
     </div>
   );
 };
 
 export default PetTable;
+
+export const Pagination = ({ table }: { table: Table<PetType> }) => {
+  const pageCount = table.getPageCount();
+  const currentPageIndex = table.getState().pagination.pageIndex;
+  const maxButtons = 5;
+  const halfMaxButtons = Math.floor(maxButtons / 2);
+
+  let startPageIndex = Math.max(currentPageIndex - halfMaxButtons, 0);
+  const endPageIndex = Math.min(startPageIndex + maxButtons - 1, pageCount - 1);
+  if (endPageIndex - startPageIndex < maxButtons - 1) {
+    startPageIndex = Math.max(endPageIndex - maxButtons + 1, 0);
+  }
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: "1rem",
+        marginTop: "1rem",
+      }}
+    >
+      <button
+        disabled={!table.getCanPreviousPage()}
+        onClick={() => table.previousPage()}
+      >
+        prev
+      </button>
+      {startPageIndex > 0 && (
+        <button onClick={() => table.setPageIndex(0)}>1</button>
+      )}
+      {startPageIndex > 1 && <span>...</span>}
+      {Array.from(
+        { length: endPageIndex - startPageIndex + 1 },
+        (_, i) => i + startPageIndex,
+      ).map((pageIndex) => (
+        <button
+          key={pageIndex}
+          disabled={currentPageIndex === pageIndex}
+          onClick={() => table.setPageIndex(pageIndex)}
+        >
+          {pageIndex + 1}
+        </button>
+      ))}
+      {endPageIndex < pageCount - 2 && <span>...</span>}
+      {endPageIndex < pageCount - 1 && (
+        <button onClick={() => table.setPageIndex(pageCount - 1)}>
+          {pageCount}
+        </button>
+      )}
+      <button
+        disabled={!table.getCanNextPage()}
+        onClick={() => table.nextPage()}
+      >
+        next
+      </button>
+    </div>
+  );
+};
