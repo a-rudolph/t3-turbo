7df6882a6185d0d001a6bd9d15ae706ef05d7240
a-rudolph <adam.rudolph@live.ca>
2023-09-07 18:56:06 -0400

    fetch pets thunk

--- apps/vitejs/src/routes/root.tsx
+++ apps/vitejs/src/routes/root.tsx
@@ -1,5 +1,4 @@
 import { useEffect } from "react";
-import { type Dispatch } from "@reduxjs/toolkit";
 import {
   Form,
   NavLink,
@@ -9,58 +8,17 @@ import {
   type LoaderFunctionArgs,
 } from "react-router-dom";
 
-import { findPetsByStatus } from "@acme/gen-swag";
-
 import { debounce } from "../lib/debounce";
 import { useTypedLoader } from "../lib/useTypedLoader";
 import { dispatch } from "../store";
 import { useAppSelector } from "../store/hooks";
-import { setPets } from "../store/pets.slice";
-
-export async function getPets({ query }: { query?: string | null }) {
-  const response = await findPetsByStatus(["available"]);
-
-  if (!response.data) {
-    throw new Error("Failed to load pets");
-  }
-
-  const pets = response.data;
-
-  const uniquePetIds = new Set<number>();
-
-  const uniquePets = pets.filter((pet) => {
-    if (
-      query &&
-      pet.name &&
-      !pet.name.toLowerCase().includes(query.toLowerCase())
-    ) {
-      return false;
-    }
-
-    if (!pet.id) return false;
-
-    if (uniquePetIds.has(pet.id)) {
-      return false;
-    } else {
-      uniquePetIds.add(pet.id);
-      return true;
-    }
-  });
-
-  return { pets: uniquePets };
-}
-
-const getPetsThunk = (query: string | null) => async (dispatch: Dispatch) => {
-  const { pets } = await getPets({ query });
-
-  dispatch(setPets(pets));
-};
+import { fetchPetsThunk } from "../store/pets.slice";
 
 export function listLoader({ request }: LoaderFunctionArgs) {
   const url = new URL(request.url);
   const query = url.searchParams.get("q");
 
-  void dispatch(getPetsThunk(query));
+  void dispatch(fetchPetsThunk(query));
 
   return { query };
 }
--- apps/vitejs/src/store/pets.slice.ts
+++ apps/vitejs/src/store/pets.slice.ts
@@ -1,6 +1,21 @@
-import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+import {
+  createAsyncThunk,
+  createSlice,
+  type PayloadAction,
+} from "@reduxjs/toolkit";
 
 import { type PetType } from "@acme/gen-swag";
+
+import { getPets } from "../services/get-pets";
+
+export const fetchPetsThunk = createAsyncThunk(
+  "pets/fetchPets",
+  async (query?: string | null) => {
+    const response = await getPets({ query });
+
+    return { pets: response.pets };
+  },
+);
 
 const petsSlice = createSlice({
   name: "pets",
@@ -11,6 +26,28 @@ const petsSlice = createSlice({
     setPets: (state, action: PayloadAction<PetType[]>) => {
       state.pets = action.payload;
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchPetsThunk.fulfilled, (state, action) => {
+        state.pets = action.payload.pets;
+      })
+      .addMatcher(
+        (action: { type: string }) => {
+          return action.type.endsWith("/pending");
+        },
+        (state, action) => {
+          console.log("start loading ", action.type);
+        },
+      )
+      .addMatcher(
+        (action: { type: string }) => {
+          return action.type.endsWith("/fulfilled");
+        },
+        (state, action) => {
+          console.log("end loading ", action.type);
+        },
+      );
   },
 });
 
